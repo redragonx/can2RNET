70b039d9ef4daeac998a730d86df82e0e7bbf3b1
Stephen Chavez <stephen@dicesoft.net>
2016-09-17 20:42:36 -0600

    fixes!

--- JoyLocal.py
+++ JoyLocal.py
@@ -226,7 +226,7 @@ def inject_rnet_joystick_frame(can_socket, rnet_joystick_id):
 	while rnet_threads_running:
             cf, addr = can_socket.recvfrom(16)
 
-            new_rnet_joystick_frame = rnet_joystick_id + '#' + dec2hex(joystick_x, 2) + dec2hex(joystick_y, 2))
+            new_rnet_joystick_frame = rnet_joystick_id + '#' + dec2hex(joystick_x, 2) + dec2hex(joystick_y, 2)
             if cf == rnet_joystick_frame_raw:
                 cansend(s, new_rnet_joystick_frame)
 			
