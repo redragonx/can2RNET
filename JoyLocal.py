6c59ad7d26a21ddd0edf90d524a31cb154f83f11
Yash Shah <yashshah0127@gmail.com>
2016-12-10 12:52:34 -0800

    Normalize indentation to 4 spaces

--- JoyLocal.py
+++ JoyLocal.py
@@ -177,15 +177,15 @@ class X360:
                 if jtype & 0x02:
                     axis = self.axis_map[jnumber]
                     if (axis == 'x'):
-                            if abs(jvalue) > self.xthreshold:
-                                    joystick_x = 0x100 + int(jvalue * 100 / 128) >> 8 &0xFF
-                            else:
-                                    joystick_x = 0
-                    elif (axis == 'y'):
-                            if abs(jvalue) > self.ythreshold:
-                                    joystick_y = 0x100 - int(jvalue * 100 / 128) >> 8 &0xFF
-                            else:
-                                    joystick_y = 0
+                        if abs(jvalue) > self.xthreshold:
+                            joystick_x = 0x100 + int(jvalue * 100 / 128) >> 8 &0xFF
+                        else:
+                            joystick_x = 0
+                elif (axis == 'y'):
+                        if abs(jvalue) > self.ythreshold:
+                            joystick_y = 0x100 - int(jvalue * 100 / 128) >> 8 &0xFF
+                        else:
+                            joystick_y = 0
 
             except:
                 print("Error reading USB joystick")
@@ -193,7 +193,7 @@ class X360:
                 joystick_y = 0
                 rnet_threads_running = False
 
-def dec2hex(dec,hexlen):  #convert dec to hex with leading 0s and no '0x' 
+def dec2hex(dec,hexlen):  #convert dec to hex with leading 0s and no '0x'
     h=hex(int(dec))[2:]
     l=len(h)
     if h[l-1]=="L":
@@ -205,19 +205,19 @@ def dec2hex(dec,hexlen):  #convert dec to hex with leading 0s and no '0x'
 #THREAD: sends RnetJoyFrame every mintime seconds
 #Not used
 def send_joystick_canframe(s,joy_id):
-        mintime = .01
-        nexttime = time() + mintime
-        priorjoystick_x=joystick_x
-        priorjoystick_y=joystick_y
-        while rnet_threads_running:
-                joyframe = joy_id+'#'+dec2hex(joystick_x,2)+dec2hex(joystick_y,2)
-                cansend(s,joyframe)
-                nexttime += mintime
-                t= time()
-                if t < nexttime:
-                    sleep(nexttime - t)
-                else:
-                    nexttime += mintime
+    mintime = .01
+    nexttime = time() + mintime
+    priorjoystick_x=joystick_x
+    priorjoystick_y=joystick_y
+    while rnet_threads_running:
+        joyframe = joy_id+'#'+dec2hex(joystick_x,2)+dec2hex(joystick_y,2)
+        cansend(s,joyframe)
+        nexttime += mintime
+        t= time()
+        if t < nexttime:
+            sleep(nexttime - t)
+        else:
+            nexttime += mintime
 
 #THREAD: Waits for joyframe and injects another spoofed frame ASAP
 def inject_rnet_joystick_frame(can_socket, rnet_joystick_id):
@@ -226,7 +226,7 @@ def inject_rnet_joystick_frame(can_socket, rnet_joystick_id):
 		cf, addr = can_socket.recvfrom(16)
 		if cf == rnet_joystick_frame_raw:
 			cansend(can_socket, rnet_joystick_id + '#' + dec2hex(joystick_x, 2) + dec2hex(joystick_y, 2))
-			
+
 
 #Waits for any frame containing a Joystick position
 #Returns: JoyFrame extendedID as text
@@ -244,26 +244,26 @@ def wait_rnet_joystick_frame(can_socket, start_time):
 
 #Set speed_range: 0% - 100%
 def RNETsetSpeedRange(cansocket,speed_range):
-        if speed_range>=0 and speed_range<=0x64:
-            cansend(cansocket,'0a040100#'+dec2hex(speed_range,2))
-        else:
-            print('Invalid RNET SpeedRange: ' + str(speed_range))
+    if speed_range>=0 and speed_range<=0x64:
+        cansend(cansocket,'0a040100#'+dec2hex(speed_range,2))
+    else:
+        print('Invalid RNET SpeedRange: ' + str(speed_range))
 
 def RNETshortBeep(cansocket):
-        cansend(cansocket,"181c0100#0260000000000000")
+    cansend(cansocket,"181c0100#0260000000000000")
 
 #Play little song
 def RNETplaysong(cansocket):
-        cansend(cansocket,"181C0100#2056080010560858")
-        sleep(.77)
-        cansend(cansocket,"181C0100#105a205b00000000")
+    cansend(cansocket,"181C0100#2056080010560858")
+    sleep(.77)
+    cansend(cansocket,"181C0100#105a205b00000000")
 
 #do very little and output something as sign-of-life
 def watch_and_wait():
-        started_time = time()
-        while threading.active_count() > 0 and rnet_threads_running:
-             sleep(0.5)
-             print(str(round(time()-started_time,2))+'\tX: '+dec2hex(joystick_x,2)+'\tY: '+dec2hex(joystick_y,2)+ '\tThreads: '+str(threading.active_count()))
+    started_time = time()
+    while threading.active_count() > 0 and rnet_threads_running:
+        sleep(0.5)
+        print(str(round(time()-started_time,2))+'\tX: '+dec2hex(joystick_x,2)+'\tY: '+dec2hex(joystick_y,2)+ '\tThreads: '+str(threading.active_count()))
 
 #does not use a thread queue.  Instead just sets a global flag.
 def kill_rnet_threads():
@@ -273,52 +273,52 @@ def kill_rnet_threads():
 
 
 if __name__ == "__main__":
-        global rnet_threads_running
-        global joystick_x
-        global joystick_y
-        rnet_threads_running = True
-        can_socket = opencansocket(0)
+    global rnet_threads_running
+    global joystick_x
+    global joystick_y
+    rnet_threads_running = True
+    can_socket = opencansocket(0)
 
-        #init usb joystick
-        x360 = X360()
-        usb_joystick_dev = x360.init_joystick()
-        joystick_x = 0
-        joystick_y = 0
+    #init usb joystick
+    x360 = X360()
+    usb_joystick_dev = x360.init_joystick()
+    joystick_x = 0
+    joystick_y = 0
 
-        if usb_joystick_dev != '':
-            print('Using USB joystick @ ' + str(usb_joystick_dev).split("'")[1])
-            usb_joystick_read_thread = threading.Thread(
-                    target=x360.usb_joystick_read_thread,
-                    args=(usb_joystick_dev,),
-                    daemon=True)
-            usb_joystick_read_thread.start()
+    if usb_joystick_dev != '':
+        print('Using USB joystick @ ' + str(usb_joystick_dev).split("'")[1])
+        usb_joystick_read_thread = threading.Thread(
+            target=x360.usb_joystick_read_thread,
+            args=(usb_joystick_dev,),
+            daemon=True)
+        usb_joystick_read_thread.start()
 
-            start_time = time() + .20
-            print('Waiting for RNET-Joystick frame')
-            rnet_joystick_id = wait_rnet_joystick_frame(can_socket, start_time) #t=timeout time
+        start_time = time() + .20
+        print('Waiting for RNET-Joystick frame')
+        rnet_joystick_id = wait_rnet_joystick_frame(can_socket, start_time) #t=timeout time
 
-            if rnet_joystick_id == 'Err!':
-                print('No RNET-Joystick frame seen within minimum time')
-                sys.exit()
+        if rnet_joystick_id == 'Err!':
+            print('No RNET-Joystick frame seen within minimum time')
+            sys.exit()
 
-            print('Found RNET-Joystick frame: ' + rnet_joystick_id)
-            
-            # set chair's speed to the lowest setting.
-            chair_speed_range = 00
-            RNETsetSpeedRange(can_socket, chair_speed_range)
-            inject_rnet_joystick_frame_thread = threading.Thread(
-                    target=inject_rnet_joystick_frame,
-                    args=(can_socket, rnet_joystick_id,),
-                    daemon=True)
-            inject_rnet_joystick_frame_thread.start()
-            sleep(0.5)
+        print('Found RNET-Joystick frame: ' + rnet_joystick_id)
+
+        # set chair's speed to the lowest setting.
+        chair_speed_range = 00
+        RNETsetSpeedRange(can_socket, chair_speed_range)
+        inject_rnet_joystick_frame_thread = threading.Thread(
+            target=inject_rnet_joystick_frame,
+            args=(can_socket, rnet_joystick_id,),
+            daemon=True)
+        inject_rnet_joystick_frame_thread.start()
+        sleep(0.5)
 
 
-            watch_and_wait()
-            kill_rnet_threads()
-        else:
-            print('No Joystick found.')
-            kill_rnet_threads()
+        watch_and_wait()
+        kill_rnet_threads()
+    else:
+        print('No Joystick found.')
+        kill_rnet_threads()
 
-                                                           
-        print("Exiting")
+
+    print("Exiting")
